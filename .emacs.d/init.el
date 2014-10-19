543e45dbfe49d7cc534c46f4f4841f8959ad0566
Wojciech Gawronski <afronski@gmail.com>
2014-10-19 20:50:31 +0200

    Configuring Haskell mode in Emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -130,3 +130,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Haskell Mode
+(add-hook 'haskell-mode-hook 'haskell-indent-mode)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
