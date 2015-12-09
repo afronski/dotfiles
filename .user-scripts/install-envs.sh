aac4e643a1f7924b246b24fe7beeedd9feb5b55d
Wojciech Gawronski <afronski@gmail.com>
2015-12-09 20:27:03 +0100

    Adding default Node.js version.

--- .user-scripts/install-envs.sh
+++ .user-scripts/install-envs.sh
@@ -9,6 +9,8 @@ nvm install 4.0
 nvm install 4.1
 nvm install 5.0
 
+nvm aliast default 0.12
+
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
