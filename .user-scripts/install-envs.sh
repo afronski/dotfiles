2bcd93a3b22845c2c1810bd35e219b9425c3aa7f
Wojciech Gawronski <afronski@gmail.com>
2015-12-09 19:46:18 +0100

    Adding updates regarding installation.

---
+++ .user-scripts/install-envs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+
+nvm install 0.8
+nvm install 0.10
+nvm install 0.12
+nvm install 4.0
+nvm install 4.1
+nvm install 5.0
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+
+rvm install 1.9.3
+rvm install 2.0
+rvm install 2.2
+rvm install jruby
+
+git clone https://github.com/gcuisinier/jenv.git ~/.jenv
+
+curl -O https://raw.githubusercontent.com/yrashk/kerl/master/kerl
+kerl build 18.1 18.1
+mkdir /home/afronski/.kerl/erlangs
+kerl install 18.1 /home/afronski/.kerl/erlangs
+
+\curl -sSL https://raw.githubusercontent.com/taylor/kiex/master/install | bash -s
+kiex install 1.1.1
+
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+dnvm upgrade -r coreclr
+
+curl -sL https://raw.githubusercontent.com/brainsik/virtualenv-burrito/master/virtualenv-burrito.sh | $SHELL
--- .xmonad/install.sh
+++ .xmonad/install.sh
@@ -3,3 +3,4 @@ cabal update
 cabal install xmonad
 cabal install xmonad-contrib --flags="-use_xft"
 cabal install xmobar --flags="with_xft with_mpd with_utf8 with_iwlib"
+cabal install yeganesh
