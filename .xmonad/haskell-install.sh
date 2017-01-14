9b8563b49a08c18ab836e16eb9a0f4828cad3631
Wojciech Gawronski <afronski@gmail.com>
2017-01-14 19:05:25 +0100

    Haskell installation script.

--- .xmonad/haskell-install.sh
+++ .xmonad/haskell-install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cabal update
+
 cabal install xmonad
 cabal install xmonad-contrib --flags="-use_xft"
+
 cabal install xmobar --flags="with_xft with_mpd with_utf8 with_iwlib"
+
 cabal install yeganesh
+
+cabal install hledger
