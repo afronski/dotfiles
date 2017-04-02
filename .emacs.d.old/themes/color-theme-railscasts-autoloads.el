75fd5f3d00debc6c3105ce7c800436e794b21544
Wojciech Gawronski <afronski@gmail.com>
2017-04-02 20:30:14 +0200

    Removing .emacs.old (Prelude).

--- .emacs.d.old/themes/color-theme-railscasts-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; color-theme-railscasts-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("color-theme-railscasts.el") (21599 55171
-;;;;;;  245267 369000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; color-theme-railscasts-autoloads.el ends here
--- .emacs.d.old/themes/solarized-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "solarized-theme" "20130920.2236" "The Solarized color theme, ported to Emacs. [github]" 'nil)
-
