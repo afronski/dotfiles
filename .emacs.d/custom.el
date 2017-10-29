a55ec85f77d9cf666e3e6dee088bb81c1177966c
Wojciech Gawronski <afronski@gmail.com>
2017-10-29 18:38:51 +0100

    [make] Updating dotfiles (2 files).

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-journal solarized-theme org-bullets nlinum helm evil-org evil-leader))))
+    (org-capture org-journal solarized-theme org-bullets nlinum helm evil-org evil-leader))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,7 +45,6 @@
  'org-bullets
  'org-journal)
 
-
 (load-theme 'solarized-dark t)
 
 (require 'org)
@@ -54,6 +53,7 @@
 
 (setq backup-directory-alist `(("." . "~/.emacs.d/backup")))
 
+(setq org-directory "~/.org")
 (setq org-default-notes-file "~/.org/notes/notes.org")
 (setq org-agenda-files (list "~/.org/notes/backlog.org"))
 (setq org-log-done t)
@@ -64,6 +64,48 @@
 
 (setq org-journal-file-format "%Y-%m-%d.org")
 (setq org-journal-date-format "%Y-%m-%d (week %V)")
+
+(define-key global-map "\C-cc" 'org-capture)
+
+(defun read-lines (filename)
+  "Return a list of lines of a file at `filename`."
+  (with-temp-buffer
+    (insert-file-contents filename)
+    (split-string (buffer-string) "\n" t)))
+
+(defun list-ledger-meta-data ()
+  (mapcar (lambda (line) (string-remove-prefix "account " line))
+	  (cl-remove-if (lambda (line) (string= "" line))
+			(read-lines "~/.org/accounting/meta.journal"))))
+
+(defun ledger-meta-data ()
+  (string-join (list-ledger-meta-data) "|"))
+
+(defun capture-ledger-file-name ()
+  "Function that should leave point at the end of a ledger file that we asked for."
+  (let ((name (read-string "Ledger file name: ")))
+    (find-file (expand-file-name (format "%s.journal" name) "~/.org/accounting"))
+    (end-of-buffer)))
+
+(setq org-capture-templates
+      '(
+	("t" "New task" entry (file+headline "~/.org/notes/backlog.org" "Not Reviewed Tasks")
+	 "** TODO %?\n   %i\n   %a")
+
+	("n" "New note" entry (file+headline "~/.org/notes/notes.org" "Not Reviewed Notes")
+	 "** [%<%Y-%m-%d>] %^{Note title}%? :%^G:\n   %^{Content}\n   %i\n   %a")
+
+	("c" "New code snippet" entry (file+headline "~/.org/notes/notes.org" "Not Reviewed Code Snippets")
+	 "** [%<%Y-%m-%d>] %^{Snippet title}%? :%^G:\n   %^{Content}\n\n   #+BEGIN_SRC %^{Languge| |bash|zsh|yaml|json|cloudformation|erlang|elixir|go}\n   %i\n   #+END_SRC\n   %a")
+
+	("l" "New ledger entry" plain (function capture-ledger-file-name)
+	 "%<%Y-%m-%d> %^{Ledger entry title}%?\n  %^{What|Expenses:Buying:Food:Groceries|%(ledger-meta-data)}  %^{Amount} %^{Currency|PLN|EUR|USD|NOK}\n  %^{From which account|Assets:Account|%(ledger-meta-data)}"
+	 :empty-lines 1)
+
+	("a" "New account for ledger" plain (file "~/.org/accounting/meta.journal")
+	 "account %^{Account name}"
+	 :empty-lines 1)
+	))
 
 (helm-mode 1)
 
